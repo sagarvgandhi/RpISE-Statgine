3e6dc410244dde2df0058f5d86062b0b280abb6c
Viren Rangaraj <34333668+viren710@users.noreply.github.com>
2017-12-11 03:19:58 +0530

    Update Range.py

--- Core/Range.py
+++ Core/Range.py
@@ -1,1 +1,18 @@
-
+    list=[2,5,4,8,7,6,9]
+  
+    def findMax(l):    #algo to find the greatest no. in a list
+      max=l[0]
+      for i in l:
+        if i>max:
+          max=i
+      return max   
+    
+    def findMin(l):    #algo to find the smallest no. in a list
+      min=l[0]
+      for i in l:
+        if i<min:
+          min=i
+      return min
+    
+    def findRange(l):
+      return findMax(list)-findMin(list)
