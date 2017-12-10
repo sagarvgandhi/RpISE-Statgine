c59a8d53db52851c2b39379d7b1994f09795f552
Viren Rangaraj <34333668+viren710@users.noreply.github.com>
2017-12-11 01:31:05 +0530

    Update Frequency.py

--- Core/Frequency.py
+++ Core/Frequency.py
@@ -1,1 +1,7 @@
-
+    def findFreq(l,x):    #the list along with a no. whose freq we wish to find in the list are passed as the two arguments
+      freq=0
+      for i in l:
+        if i==x:
+          freq=freq+1     #freq counter is incremented every time a match is found
+      return x,freq       #Returns the no. with it's corresponding freq
+    
