f420b7fa1c027394474272b2eb21185c648d8345
Sai Sravan <sravanmunukutla@gmail.com>
2017-12-11 17:40:57 +0530

    Update Mean.py

--- Core/Mean.py
+++ Core/Mean.py
@@ -1,10 +1,7 @@
 data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-def mean(data):
-    total = 0                       
-    
-    listVar = data            
-    listVar.sort()            #Sorting of list for mean calculation is not a necessity, but done anyway
+def mean(listVar):
+    total = 0 
 
     for i in listVar:         #Each element from list gets added to find total
         total += i
