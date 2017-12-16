7d9b48f935c995ae532f9b000f67c20938cce546
Viren Rangaraj <34333668+viren710@users.noreply.github.com>
2017-12-16 19:07:57 +0530

    Update codes.py

--- viren/codes.py
+++ viren/codes.py
@@ -64,5 +64,22 @@
       
       #PROGRAM FOR RANGE
       
+       def findMax(l):    #algo to find the greatest no. in a list
+      max=l[0]
+      for i in l:
+        if i>max:
+          max=i
+      return max   
+    
+    def findMin(l):    #algo to find the smallest no. in a list
+      min=l[0]
+      for i in l:
+        if i<min:
+          min=i
+      return min
+    
+    def findRange(l):
+      return findMax(list)-findMin(list)
+      
       
       
